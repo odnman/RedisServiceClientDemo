b05cfc66891f513a9af3c4e6c0ba8efb71b17512
ODN MAN <oduntane34.5@gmail.com>
2022-06-29 21:50:36 +0100

    Redis Docker Fix 1

--- redisclient.js
+++ redisclient.js
@@ -6,7 +6,7 @@ const redis = require("redis");
 
 const redisClient = redis.createClient({
     host: process.env.REDIS_HOST,
-    port: process.env.REDIST_PORT
+    port: process.env.REDIS_PORT
 });
 
 redisClient.on("error", function(err) {
